9bf3dc6c0afb9d82ca35fbb490655cbf7c78d61a
Silinou Schneider <enrickminko@gmail.com>
2026-08-03 10:57:47 +0100

    urls

--- auth-service/config/urls.py
+++ auth-service/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView 
 from users.views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -7,6 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register', RegisterView.as_view(), name='register'),
     path('api/login', TokenObtainPairView.as_view(), name='login'),
+    
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
     path('api/v1/auth/', include('users.urls')),
 ]
